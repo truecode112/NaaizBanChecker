6fc6aa4fa98acd83593aa45ac642d26664d813dd
Alexander <Alexander@assemblysoft.com>
2022-11-28 07:10:09 +0100

    Initial commit

---
+++ accountChecker/AccountChecker.java
@@ -0,0 +1,17 @@
+package accountChecker;
+
+import org.osbot.rs07.script.Script;
+
+public class AccountChecker extends Script {
+    @Override
+    public void onStart() throws InterruptedException {
+        // TODO Auto-generated method stub
+        super.onStart();
+    }
+
+    @Override
+    public int onLoop() throws InterruptedException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
